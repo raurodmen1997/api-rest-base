34f8759c348dcb41142ddf3f36f543702c4bc0ad
raurodmen1997 <raulrodriguezmendez1997@gmail.com>
2021-11-28 09:11:53 +0100

    Create proyect

---
+++ src/main/java/api/rest/base/ApiRestBaseApplication.java
@@ -0,0 +1,13 @@
+package api.rest.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/rest/base/ApiRestBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package api.rest.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
